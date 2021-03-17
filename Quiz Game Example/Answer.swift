49edb8921e46f181cd046b0622734a0323eefaa9
Shanice Brown <s.mariebrown10@gmail.com>
2021-03-17 10:50:03 -0500

    possible code for quiz questions

---
+++ Quiz Game Example/Answer.swift
@@ -0,0 +1,16 @@
+//
+//  Answer.swift
+//  Quiz Game Example
+//
+//  Created by Shanice Brown (WHA) on 3/17/21.
+//
+
+import Foundation
+
+struct Answer {
+    var possibleAnswers: [Answer]
+    var fillInBlankAnswer: String
+    var correctAnswer: Bool
+    
+}
+
---
+++ Quiz Game Example/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  Quiz Game Example
+//
+//  Created by Shanice Brown (WHA) on 3/17/21.
+//
+
+import Foundation
+
+struct Question {
+    var questionID: Int
+    var questionText: String
+   
+}
+
