bfb05883bb43c5db8463a37d592c0c26bf74983e
Shanice Brown <s.mariebrown10@gmail.com>
2021-03-16 22:34:18 -0500

    Initial Commit

---
+++ Quiz Game Example/Quiz_Game_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_Game_ExampleApp.swift
+//  Quiz Game Example
+//
+//  Created by Shanice Brown (WHA) on 3/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_Game_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
